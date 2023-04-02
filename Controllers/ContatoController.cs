63d9f1513c49192daccf9fd9022e7b533db0f83c
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-02 14:11:37 -0300

    criação do controller para create, update e delete de contatos

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -16,5 +16,19 @@ namespace ContactRegister.Controllers
             return View();
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
     }
 }
