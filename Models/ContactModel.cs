d96b2e51c9c4ebda85a9cb00fef07104bdf372f2
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-04 16:36:32 -0300

    mudança no funcionamento do guid

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -2,16 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContactRegister.Models
 {
     public class ContactModel
     {
+        
         public Guid Id
         {
             get;
             set;
-        }
+        } = Guid.NewGuid();
 
         public string Name
         {
