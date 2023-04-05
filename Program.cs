632980e3876b33eccb5ff89aaa434d10e168e2cf
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-05 00:09:13 -0300

    injeçãode dependencia relativa ao repositorio

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using ContactRegister.Data;
+using ContactRegister.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Configuração de acesso ao banco de dados
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContextPool<DataBaseContext>(options => 
                 options.UseMySql(mySqlConnection, 
                     ServerVersion.AutoDetect(mySqlConnection)));
+
+// Injeção de Dependencia
+// https://learn.microsoft.com/pt-br/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-6.0
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
 
 builder.Services.AddControllersWithViews();
 
