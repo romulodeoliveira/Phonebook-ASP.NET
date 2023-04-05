d8f8a6d91d92b53e6884d8c642f73a61c1a545f4
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-05 00:08:30 -0300

    criação do repositorio para contato

---
+++ Repositories/ContactRepository.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactRegister.Models;
+using ContactRegister.Data;
+
+namespace ContactRegister.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+
+        // referencia em vídeo:
+        // https://youtu.be/8pkGQKuW6Ss
+        // a partir do minuto 30:00
+
+        private readonly DataBaseContext _dataBaseContext;
+        public ContactRepository(DataBaseContext dataBaseContext)
+        {
+            _dataBaseContext = dataBaseContext;
+        }
+
+        public ContactModel ToAdd(ContactModel contact)
+        {
+            // aqui gravo no banco de dados
+            _dataBaseContext.Contacts.Add(contact);
+            _dataBaseContext.SaveChanges();
+            return contact;
+        }
+    }
+}
