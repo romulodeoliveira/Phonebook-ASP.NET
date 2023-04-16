382d8801fb637973010c86e3c67f2d8e9bb2cae0
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-16 17:49:28 -0300

    desenvolvimento da tabela de usuários

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,11 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Project01.Enums;
 
-namespace Project01.Models
+namespace ContactRegister.Models
 {
     public class UserModel
     {
+        public ProfileEnum Profile { get; set; }
 
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public UserModel(ProfileEnum profile)
+        {
+            Profile = profile;
+
+            string typeUser = "";
+
+            if (Profile == ProfileEnum.ADM)
+            {
+                typeUser = "ADM";
+            }
+
+            else if (Profile == ProfileEnum.MEM)
+            {
+                typeUser = "MEM";
+            }
+
+            Id = $"USER-{Guid.NewGuid():N}-{typeUser}";
+        }
     }
 }
