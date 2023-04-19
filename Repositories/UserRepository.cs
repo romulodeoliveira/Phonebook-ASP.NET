eb8c4594995f5f36a5db049ac265070862eb55f3
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-19 20:48:45 -0300

    implementando o repositorio de usuarios para o CRUD

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactRegister.Models;
+using ContactRegister.Data;
+
+namespace ContactRegister.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+        // referencia em vídeo:
+        // https://youtu.be/8pkGQKuW6Ss
+        // a partir do minuto 30:00
+
+        private readonly DataBaseContext _dataBaseContext;
+        public UserRepository(DataBaseContext dataBaseContext)
+        {
+            _dataBaseContext = dataBaseContext;
+        }
+
+        public UserModel ListById(string id)
+        {
+            return _dataBaseContext.Users.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<UserModel> FindAll()
+        {
+            return _dataBaseContext.Users.ToList();
+        }
+
+        public UserModel ToAdd(UserModel user)
+        {
+            // aqui gravo no banco de dados
+            _dataBaseContext.Users.Add(user);
+            _dataBaseContext.SaveChanges();
+            return user;
+        }
+
+        public UserModel ToUpdate(UserModel user)
+        {
+            UserModel userDB = ListById(user.Id);
+
+            if (userDB == null) throw new System.Exception("Houve um erro na atualização do usuário!");
+
+            userDB.FirstName = user.FirstName;
+            userDB.MiddleName = user.MiddleName;
+            userDB.LastName = user.LastName;
+            userDB.UserName = user.UserName;
+            userDB.Email = user.Email;
+
+            _dataBaseContext.Users.Update(userDB);
+            _dataBaseContext.SaveChanges();
+
+            return userDB;
+        }
+
+        public bool ToDelete(string id)
+        {
+            UserModel userDB = ListById(id);
+
+            if (userDB == null) throw new System.Exception("Houve um erro na exclusão do contato!");
+
+            _dataBaseContext.Users.Remove(userDB);
+            _dataBaseContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
