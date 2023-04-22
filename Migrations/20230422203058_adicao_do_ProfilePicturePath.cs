79a7ad2901c88c26f094eb9042871cb3516322b7
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-22 18:45:47 -0300

    migração para inclusão do caminho das fotos de perfis no banco de dados

---
+++ Migrations/20230422203058_adicao_do_ProfilePicturePath.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactRegister.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicao_do_ProfilePicturePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "char(36)",
+                nullable: false,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicturePath",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicturePath",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+        }
+    }
+}
