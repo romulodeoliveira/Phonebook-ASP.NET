72d80c3b0acd16f3221153220803fd1a4db08969
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-04-23 17:45:51 -0300

    passando a anotação criada na classe UniqueAttribute nos campos username e email e tornando o campo email requerido

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Project01.Enums;
+using System.ComponentModel.DataAnnotations;
+using Project01.Utils;
 
 namespace ContactRegister.Models
 {
@@ -20,10 +22,16 @@ namespace ContactRegister.Models
 
         public string? ProfilePicturePath { get; set; }
 
+        [Required(ErrorMessage = "Digite o seu nome de usuário!")]
+        [Unique(ErrorMessage = "Este nome de usuário já está em uso.")]
         public string UserName { get; set; }
 
-        public string? Email { get; set; }
+        [Required(ErrorMessage = "Digite o e-mail!")]
+        [EmailAddress(ErrorMessage = "O e-mail informado é inválido!")]
+        [Unique(ErrorMessage = "Este e-mail já está em uso.")]
+        public string Email { get; set; }
 
+        [Required(ErrorMessage = "Digite a senha!")]
         public string Password { get; set; }
 
         public DateTime CreatedAt { get; set; }
