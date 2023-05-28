c7be70cda53d80a575d88e024090a6399193e1bc
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-28 17:45:01 -0300

    adição do campo ImagePath para salvar o caminho da imagem no banco de dados

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -9,35 +9,21 @@ namespace ContactRegister.Models
 {
     public class ContactModel
     {
-        
-        public Guid Id
-        {
-            get;
-            set;
-        } = Guid.NewGuid();
+
+        public Guid Id { get; set; } = Guid.NewGuid();
 
         [Required(ErrorMessage = "Digite o nome!")]
-        public string? Name
-        {
-            get;
-            set;
-        }
+        public string? Name { get; set; }
 
         [Required(ErrorMessage = "Digite o e-mail!")]
         [EmailAddress(ErrorMessage = "O e-mail informado é inválido!")]
-        public string? Email
-        {
-            get;
-            set;
-        }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Digite o número de telefone!")]
         [Phone(ErrorMessage = "O número de telefone informado não é válido!")]
-        public string? PhoneNumber
-        {
-            get;
-            set;
-        }
+        public string? PhoneNumber { get; set; }
+
+        public string? ImagePath { get; set; }
 
     }
 }
