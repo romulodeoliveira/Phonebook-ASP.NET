bab58f717e98f7c66349bceda462cb4f2967c637
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-06-21 03:41:22 -0300

    criação de uma chave secreta

---
+++ Helpers/Settings.cs
@@ -0,0 +1,8 @@
+namespace Phonebook.Helpers;
+
+public class Settings
+{
+    // https://youtu.be/vAUXU0YIWlU
+    // https://balta.io/artigos/aspnet-5-autenticacao-autorizacao-bearer-jwt
+    public static string Secret = "RCC&KyDU4m@aKG&yQe$PWJqV%ZEDzUYWK@*!aHmmVs^Vi@#!Vd!p9y8cP@5Qn!%6JAuNjYMrJANR$n!hv!SEsL3a$oVJuL%EHnASEp!7r%5u#qe!kiZ$shi^C9C6c@B8";
+}
